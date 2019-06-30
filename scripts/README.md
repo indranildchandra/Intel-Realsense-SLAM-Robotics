365dd752dc2a307bcb316152c5a3bc9bc5a1f324
Laukik Hase <laukik17@gmail.com>
2019-06-30 14:19:48 +0530

    Get Started

---
+++ scripts/README.md
@@ -0,0 +1,11 @@
+# SLAM using Intel Realsense d435i
+
+For installation, run the installation script as:
+
+```bash
+./install16.sh
+```
+This script installs the following packages:
+1) Intel Realsense SDK
+2) Realsense ROS
+3) RTAB-Map ros-pkg alongwith RTAB-Map standalone libraries
---
+++ scripts/install16.sh
@@ -0,0 +1,62 @@
+#Update Keys
+
+sudo apt-key del 421C365BD9FF1F717815A3895523BAEEB01FA116
+sudo -E apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
+sudo apt-get update
+sudo apt-get upgrade
+
+#Intel Realsense SDK
+
+sudo apt-key adv --keyserver keys.gnupg.net --recv-key C8B3A55A6F3EFCDE || sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-key C8B3A55A6F3EFCDE
+sudo add-apt-repository "deb http://realsense-hw-public.s3.amazonaws.com/Debian/apt-repo xenial main" -u
+sudo apt-get install librealsense2-dkms
+sudo apt-get install librealsense2-utils
+sudo apt-get update
+sudo apt-get upgrade
+
+#Realsense Firmware Update
+
+#dynamicreconfigure dependency package
+
+cd ~
+cd catkin_ws/src
+git clone https://github.com/pal-robotics/ddynamic_reconfigure.git
+
+#Realsense ROS
+
+mkdir -p ~/catkin_ws/src
+cd ~/catkin_ws/src/
+git clone https://github.com/IntelRealSense/realsense-ros.git
+cd realsense-ros/
+git checkout `git tag | sort -V | grep -P "^\d+\.\d+\.\d+" | tail -1`
+cd ..
+catkin_init_workspace
+cd ..
+catkin_make clean
+catkin_make -DCATKIN_ENABLE_TESTING=False -DCMAKE_BUILD_TYPE=Release
+catkin_make install
+echo "source ~/catkin_ws/devel/setup.bash" >> ~/.bashrc
+source ~/.bashrc
+
+
+#rtabmap
+
+cd ~
+git clone https://github.com/introlab/rtabmap.git rtabmap
+cd rtabmap/build
+cmake -DCMAKE_INSTALL_PREFIX=~/catkin_ws/devel ..
+make -j2
+sudo make -j2 install
+echo "source ~/catkin_ws/devel/setup.bash" >> ~/.bashrc
+source ~/.bashrc
+
+#rtabmap_ros
+
+cd ~/catkin_ws
+git clone https://github.com/introlab/rtabmap_ros.git src/rtabmap_ros
+catkin_make -j1
+echo "source ~/catkin_ws/devel/setup.bash" >> ~/.bashrc
+source ~/.bashrc
+
+
+
